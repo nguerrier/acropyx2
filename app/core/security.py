34b03a2d197fe71cd2f7f304698435d6097afdad
Jérôme LOYET <822436+fatpat@users.noreply.github.com>
2022-05-29 08:42:08 +0200

    rename project, database + handle no auth

--- app/core/security.py
+++ app/core/security.py
@@ -1,12 +1,13 @@
 import secrets
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from core.config import settings
 
 security = HTTPBasic()
 
-def auth(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, "admin")
-    correct_password = secrets.compare_digest(credentials.password, "admin")
+def auth_http_basic(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, settings.ADMIN_USER)
+    correct_password = secrets.compare_digest(credentials.password, settings.ADMIN_PASS)
     if not (correct_username and correct_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -14,3 +15,11 @@ def auth(credentials: HTTPBasicCredentials = Depends(security)):
             headers={"WWW-Authenticate": "Basic"},
         )
     return True
+
+def no_auth():
+    return True
+
+if settings.ADMIN_USER is not None and settings.ADMIN_PASS is not None:
+    auth = auth_http_basic
+else:
+    auth = no_auth
