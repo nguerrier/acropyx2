45b7c5b64af43e8cf36273d682df46714532c7b9
Jérôme LOYET <822436+fatpat@users.noreply.github.com>
2022-06-06 21:56:36 +0200

    add home page

--- back/routers/api.py
+++ back/routers/api.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter
+from fastapi.responses import HTMLResponse
 
-#from app.api.api_v1.endpoints import items, login, users, utils
 from routers.status import status
 from routers.pilots import pilots
 from routers.judges import judges
 from routers.teams import teams
 from routers.tricks import tricks
 from routers.competitions import competitions
+
+from core.config import settings
 
 router = APIRouter()
 router.include_router(status, prefix="/status", tags=["status"])
@@ -15,3 +17,21 @@ router.include_router(judges, prefix="/judges", tags=["judges"])
 router.include_router(teams, prefix="/teams", tags=["teams"])
 router.include_router(tricks, prefix="/tricks", tags=["tricks"])
 router.include_router(competitions, prefix="/competitions", tags=["competitions"])
+
+@router.get(
+    "/",
+    status_code=200,
+    response_description="home page",
+    response_class=HTMLResponse,
+)
+async def home():
+    return f"""
+    <html>
+        <head>
+            <title>{settings.PROJECT_NAME} version {settings.VERSION}</title>
+        </head>
+        <body>
+            <h1>{settings.PROJECT_NAME} version {settings.VERSION}</h1>
+        </body>
+    </html>
+    """
