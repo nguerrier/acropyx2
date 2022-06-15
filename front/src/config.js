491d3f64b78784843aa6e503bca3633cf87996fb
Jérôme LOYET <822436+fatpat@users.noreply.github.com>
2022-06-15 16:43:18 +0200

    remove front as it will come from nico and julien

--- front/src/config.js
+++
@@ -1,8 +0,0 @@
-//import runtimeEnv from '@mars/heroku-js-runtime-env';
-
-const config = {
-  apiBasePath: process.env.REACT_APP_API_URL,
-  reactAppMode: process.env.REACT_APP_MODE || 'dev',
-};
-
-export default config;
