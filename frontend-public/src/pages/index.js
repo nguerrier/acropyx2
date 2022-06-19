535905c552ec75edd8534a4a1b560dd13e10ebe6
Nicolas GUERRIER <n.guerrier@manageo.fr>
2022-06-19 16:06:58 +0200

    Update

--- frontend-public/src/pages/index.js
+++ frontend-public/src/pages/index.js
@@ -2,7 +2,6 @@
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 
-
 // ** Styled Component Import
 import ApexChartWrapper from 'src/@core/styles/libs/react-apexcharts'
 
