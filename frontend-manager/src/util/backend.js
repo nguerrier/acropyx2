78b26ed5ad9e08dfcf3ea29c3dea515cc403085a
Jérôme LOYET <822436+fatpat@users.noreply.github.com>
2022-06-23 09:11:22 +0200

    [manager] wrap API calls

--- frontend-manager/src/util/backend.js
+++ frontend-manager/src/util/backend.js
@@ -67,3 +67,31 @@ export async function getCompetitions(id) {
 export async function getCompetitionResults(id) {
   return await get('/competitions/' + id + '/results')
 }
+
+export async function APIRequest(route, props={}) {
+    var expected_status = props.expected_status ?? 200
+    var expect_json = props.expect_json ?? false
+
+    route = `/api/acropyx${route}`
+    const res = await fetch(route, props)
+
+    var body = null
+    const contentType = res.headers.get('content-type')
+    if (contentType && contentType.includes('application/json')) {
+      body = await res.json()
+    }
+
+    if (expect_json && body == null) {
+        err = `did not receive JSON response`
+    }
+
+    var err = null
+    if (expected_status > 0 && res.status != expected_status) {
+        err = `wrong status code (received ${res.status} while expecting ${expected_status}`
+        if (body) {
+            err += ": " + JSON.stringify(body)
+        }
+    }
+
+    return [err, body, res.headers]
+}
