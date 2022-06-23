238e719f438b7d76c4cbd913f5d501dacd4cbc13
Jérôme LOYET <822436+fatpat@users.noreply.github.com>
2022-06-23 09:11:48 +0200

    [manager] add notifications hook

---
+++ frontend-manager/src/util/notifications.js
@@ -0,0 +1,17 @@
+import { useSnackbar } from 'notistack';
+
+export const getNotifications = (props) => {
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const notification = (text, type) => {
+    const k = enqueueSnackbar(text, {
+      variant: type,
+      onClick: () => closeSnackbar(k)
+    })
+  }
+  const success = (text) => {notification(text, 'success')}
+  const error = (text) => {notification(text, 'error')}
+  const warning = (text) => {notification(text, 'warning')}
+  const info = (text) => {notification(text, 'info')}
+
+  return [success, info, warning, error]
+}
